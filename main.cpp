b33068a88f28ebdf914f6f521e90056409019fe2
Milow <milow2502@stu.cn.ua>
2023-09-23 13:41:49 +0300

    lab1

---
+++ main.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <windows.h>
+#include <cmath>
+
+using namespace std;
+
+void frameLine(int k);
+
+int main() {
+    // Налаштування для української мови
+    SetConsoleCP(65001);//utf-8
+    SetConsoleOutputCP(65001);//utf-8
+    puts("\t Розробник Бивойно П.Г., гр.ІКС-880");
+    puts("\t Дата народження 1943/08/22.");
+    puts("\t Номер у списку групи 0.");
+    puts("\t Лабораторна робота № 1");
+}
